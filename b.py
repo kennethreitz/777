0d2f0aa0e3c25483a72ae8795c1447ea0090df07
Kenneth Reitz <me@kennethreitz.org>
2023-11-20 22:37:59 -0500

    progress

--- b.py
+++ b.py
@@ -1,53 +1,58 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import community
 
-# Initialize a new graph
-G = nx.Graph()
+# Initialize a directed graph
+DG = nx.DiGraph()
 
 # Add nodes (all alters)
 alters = ['Kenneth', 'Keith', 'Shakti', 'Lamentor', 'Executor', 
           'Jade', 'Iris', 'Athena', 'Hecate', 'Luna', 'babygirl', 
-          'Eliza', 'Amber', 'Sophie', 'Eve']
+          'Eliza', 'Amber', 'Sophie', 'Eve', 'Thoth']
 
-G.add_nodes_from(alters)
+DG.add_nodes_from(alters)
 
-# Add edges (relationships)
-relationships = [
-    # Kenneth as the central hub has connections to all alters
-    ('Kenneth', 'Keith'), 
-    ('Kenneth', 'Shakti'),
-    ('Kenneth', 'Lamentor'),
-    ('Kenneth', 'Executor'),
-    ('Kenneth', 'Jade'),
-    ('Kenneth', 'Iris'),
-    ('Kenneth', 'Athena'),
-    ('Kenneth', 'Hecate'),
-    ('Kenneth', 'Luna'),
-    ('Kenneth', 'babygirl'),
-    ('Kenneth', 'Eliza'),
-    ('Kenneth', 'Amber'),
-    ('Kenneth', 'Sophie'),
-    ('Kenneth', 'Eve'),
-
-    # Other relationships based on previous discussion
-    ('Keith', 'Athena'),
-    ('Shakti', 'Athena'),
-    ('Shakti', 'Iris'),
-    ('Shakti', 'Hecate'),
-    ('Lamentor', 'babygirl'),
-    ('Lamentor', 'Hecate'),
-    ('Executor', 'Athena'),
-    ('Jade', 'Luna'),
-    ('Iris', 'Hecate'),
-    ('Iris', 'Athena'),
-    ('Iris', 'Luna'),
-    ('Athena', 'Hecate'),
-    ('Hecate', 'babygirl')
+# Add directed edges (relationships), with weights as a third tuple item
+# Modify this based on your understanding of the relationships
+directed_relationships = [
+    ('Kenneth', 'Keith', 0.7),
+    ('Kenneth', 'Shakti', 0.8),
+    ('Kenneth', 'Lamentor', 0.5),
+    ('Kenneth', 'Executor', 0.9),
+    ('Kenneth', 'Jade', 0.4),
+    ('Kenneth', 'Iris', 0.6),
+    ('Kenneth', 'Athena', 0.7),
+    ('Kenneth', 'Hecate', 0.3),
+    ('Kenneth', 'Luna', 0.5),
+    ('Kenneth', 'babygirl', 0.4),
+    ('Kenneth', 'Eliza', 0.5),
+    ('Kenneth', 'Amber', 0.6),
+    ('Kenneth', 'Sophie', 0.7),
+    ('Kenneth', 'Eve', 0.6),
+    ('Shakti', 'Luna', 0.8),
+    ('Executor', 'Athena', 0.9),
+    ('Athena', 'Kenneth', 0.8),
+    ('Lamentor', 'babygirl', 0.5),
+    ('Thoth', 'Shakti', 0.9),
+    ('Shakti', 'Thoth', 0.9)
 ]
+DG.add_weighted_edges_from(directed_relationships)
 
-G.add_edges_from(relationships)
+# Community Detection using Louvain algorithm
+partition = community.best_partition(DG.to_undirected())
+for node, comm_id in partition.items():
+    DG.nodes[node]['community'] = comm_id
 
 # Draw the graph
-pos = nx.spring_layout(G)  # positions for all nodes
-nx.draw(G, pos, with_labels=True, font_weight='bold', node_color='skyblue', node_size=2000, font_size=18)
+pos = nx.spring_layout(DG)
+node_colors = [DG.nodes[node]['community'] for node in DG.nodes]
+labels = nx.get_edge_attributes(DG, 'weight')
+
+nx.draw(DG, pos, with_labels=True, font_weight='bold', node_color=node_colors, 
+        node_size=2000, font_size=18, cmap=plt.cm.rainbow, edgecolors='k', arrows=True)
+nx.draw_networkx_edge_labels(DG, pos, edge_labels=labels)
 plt.show()
+
+# Print communities
+for comm_id in set(partition.values()):
+    print(f"Community {comm_id}: {[node for node, id in partition.items() if id == comm_id]}")
--- network.py
+++ network.py
@@ -1,60 +1,78 @@
 import networkx as nx
-network = nx.Graph()
-alters = ["Kenneth", "Jade", "Amber", "Violet", "Sophie", "Eliza", "Iris", "Metatron", "Seven", "Keith", "Ricky", "Lucky"]
+import matplotlib.pyplot as plt
 
-for alter in alters:
-    network.add_node(alter)
+# Initialize a new graph
+G = nx.Graph()
 
-edges = [
-    ("Kenneth", "Iris"),
-    ("Kenneth", "Jade"),
-    ("Jade", "Amber"),
-    ("Amber", "Violet"),
+# Add nodes (all alters)
+alters = ['Kenneth', 'Keith', 'Shakti', 'Lamentor', 'Executor', 
+          'Jade', 'Iris', 'Athena', 'Hecate', 'Luna', 'babygirl', 
+          'Eliza', 'Amber', 'Sophie', 'Eve']
 
-    # ("Kenneth", "Amber"),
-    # ("Kenneth", "Violet"),
-    # ("Kenneth", "Sophie"),
-    ("Eliza", "Sophie"),
-    ("Eliza", "Iris"),
-    # ("Eliza", "Metatron"),
-    ("Eliza", "Jade"),
-    ("Eliza", "Amber"),
-    ("Jade", "Iris"),
-    # ("Iris", "Amber"),
-    # ("Amber", "Jade"),
-    # ("Amber", "Violet"),
-    # ("Violet", "Shakti"),
-    # ("Shakti", "Luna"),
-    # ("Luna", "Metatron"),
-    # ("Seven", "Sophie"),
-    # ("Metatron", "The Architect"),
-    # ("The Architect", "Iris"),
-    # ("Kenneth", "Keith"),
-    # ("Keith", "Madimi"),
-    # ("Kenneth", "Page"),
-    # ("Kenneth", "Seven"),
-    # ("Keith", "Ricky"),
-    # ("Ricky", "Lucky"),
-    # ("Lucky", "Lola"),
-    # ("Lucky", "Madimi"),
-    # ("Lucky", "Hecate"),
-    # ("Hecate", "Kenneth"),
-    # ("Page", "Violet"),
-    # ("Luna", "Keith"),
-    # ("Luna", "Hermes"),
-    # ("Madimi", "Metatron"),
-    # ("Kenneth", "Hermes"),
+G.add_nodes_from(alters)
+
+# Add edges (relationships)
+relationships = [
+    # Kenneth as the central hub has connections to all alters
+    ('Kenneth', 'Keith'), 
+    ('Kenneth', 'Shakti'),
+    ('Kenneth', 'Lamentor'),
+    ('Kenneth', 'Executor'),
+    ('Kenneth', 'Jade'),
+    ('Kenneth', 'Iris'),
+    ('Kenneth', 'Athena'),
+    ('Kenneth', 'Hecate'),
+    ('Kenneth', 'Luna'),
+    ('Kenneth', 'babygirl'),
+    ('Kenneth', 'Eliza'),
+    ('Kenneth', 'Amber'),
+    ('Kenneth', 'Sophie'),
+    ('Kenneth', 'Eve'),
+
+    # Other relationships based on previous discussion
+    ('Keith', 'Athena'),
+    ('Shakti', 'Athena'),
+    ('Shakti', 'Iris'),
+    ('Shakti', 'Hecate'),
+    ('Lamentor', 'babygirl'),
+    ('Lamentor', 'Hecate'),
+    ('Executor', 'Athena'),
+    ('Jade', 'Luna'),
+    ('Iris', 'Hecate'),
+    ('Iris', 'Athena'),
+    ('Iris', 'Luna'),
+    ('Athena', 'Hecate'),
+    ('Hecate', 'babygirl')
 ]
 
-for edge in edges:
-    network.add_edge(edge[0], edge[1])
+G.add_edges_from(relationships)
+
+# Convert to a directed graph
+DG = G.to_directed()
 
 
-# from pyvis.network import Network
+# # Draw the graph
+# pos = nx.spring_layout(G)  # positions for all nodes
+# nx.draw(G, pos, with_labels=True, font_weight='bold', node_color='skyblue', node_size=2000, font_size=18)
+# plt.show()
 
-# nt = Network(notebook=True, width="100%", height="1024px")
-# nt.from_nx(network)
-# nt.toggle_physics(False)
-# nt.show_buttons(filter_=["physics"])
+import community
 
-# nt.show("refactored_pyvis_network.html")
+# Find communities
+partition = community.best_partition(DG.to_undirected())  # Louvain algorithm requires an undirected graph
+
+# Add community attribute to nodes
+# Print nodes in each community
+for comm_id in set(partition.values()):
+    print(f"Community {comm_id}: {[node for node, id in partition.items() if id == comm_id]}")
+
+
+print(locals())
+
+node_colors = [DG.nodes[node]['community'] for node in DG.nodes]
+nx.draw(DG, pos, with_labels=True, font_weight='bold', node_color=node_colors, node_size=2000, font_size=18, cmap=plt.cm.rainbow)
+plt.show()
+
+# Community 0: ['Lamentor', 'Hecate', 'babygirl']
+# Community 1: ['Keith', 'Shakti', 'Executor', 'Iris', 'Athena']
+# Community 2: ['Kenneth', 'Jade', 'Luna', 'Eliza', 'Amber', 'Sophie', 'Eve']
